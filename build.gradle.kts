fac0bc0759a51cc516f8073d7a29756ee1c641b2
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-08-19 19:30:49 +0200

    Hint restart parameter

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,6 +66,7 @@ tasks {
             val client = HttpClient.newHttpClient()
             val request = HttpRequest.newBuilder()
                 .uri(URI.create(apiUrl))
+                //.uri(URI.create("$apiUrl?restart=true"))
                 .header("Content-Type", "application/octet-stream")
                 .POST(HttpRequest.BodyPublishers.ofByteArray(Files.readAllBytes(filePath)))
                 .build()
